71ee0a9b5e59cd48585cb5dd1aad9ca69ebd6cc8
leismale <leismale@gmail.com>
2018-09-07 00:01:29 +0100

    Comments added

--- Answer.js
+++ Answer.js
@@ -1,7 +1,13 @@
 var $ = function (selector) {
     var elements = [];
 
-    //Just a tag
+    /*
+    Search for tags
+    Check if there are . or # in the selector, indexOf returns -1 if the value is not found in the selector
+    If there are no . or # in the selector we are searching for a tag
+    Search for the element, convert the HTML collection returned to an array so we can iterate over it
+    Finally push the result to the elements array 
+    */
     if (selector.indexOf(".") == -1 && selector.indexOf("#") == -1) {
         var foundTags = toArray(document.getElementsByTagName(selector))
         foundTags.forEach(e => {
@@ -9,27 +15,44 @@ var $ = function (selector) {
         })
     }
 
-    //Just an id
+    /*
+    Search for an id
+    Check if there is a # in the first position of the selector
+    If there is a # in the first position we are searching for an id
+    Split the selector string to get the name of the id we are searching for
+    Finally get the element we are searching for and push the result to the elements array 
+    */
     if (selector.indexOf("#") == 0) {
-        idSelector = selector.split("#")[1];
+        var idSelector = selector.split("#")[1];
         elements.push(document.getElementById(idSelector));
     }
 
-    //Just a class
+    /*
+    Search for a class
+    Checking if there is a . in the first position of the selector
+    If there is a . in the first position we are searching for a class
+    Split the selector string to get the name of the class we are searching for
+    Get the elements we are searching for, convert the HTML collection returned to an array so we can iterate over it
+    Finally push the result to the elements array 
+    */
     if (selector.indexOf(".") == 0) {
-        classSelector = selector.split(".")[1]
+        var classSelector = selector.split(".")[1]
         var foundClasses = toArray(document.getElementsByClassName(classSelector));
         foundClasses.forEach(e => {
             elements.push(e);
         })
     }
 
-    //Multiple selector Tag, id and class
+    /*
+    Multiple selector Tag, id and class
+    */
     if (selector.indexOf("#") > 0 || selector.indexOf(".") > 0) {
+        //Get the position of the # and . on the selector so we can compare them and look for a class or id first
         var sharp = selector.indexOf("#");
         var dot = selector.indexOf(".");
         var tag, id, klass;
 
+        //Split the selector and use substring to get the class/id name
         if (sharp == -1) { //Tag and class
             tag = selector.split(".")[0]
             klass = selector.split(".")[1]
@@ -48,10 +71,12 @@ var $ = function (selector) {
             }
         }
 
+        //Search for the elements, convert the HTML collections to arrays
         var foundTags = toArray(document.getElementsByTagName(tag));
         var foundId = [document.getElementById(id)];
         var foundClasses = toArray(document.getElementsByClassName(klass));
 
+        //Finally push the result to the elements array
         foundTags.forEach(e => {
             if (!id) {
                 elements.push(e)
@@ -64,10 +89,10 @@ var $ = function (selector) {
             }
         })
     }
-
     return elements;
 }
 
+//Creates an array from an HTML collection so we can iterate over it
 var toArray = function (element) {
     return Array.from(element)
 }
